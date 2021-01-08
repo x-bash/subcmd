b556c294e45808b5eb1d8d3840d8b86860b08427
edwinjhlee <edwin.jh.lee@gmail.com>
2021-01-08 22:24:29 +0800

    add json_walk_panic

--- json.sh
+++ json.sh
@@ -1,13 +1,26 @@
 #! /usr/bin/env bash
 
+# . json.sh; json.extract
+
 json_extract(){
-    local code
-    code=$(cat "./json_walk" "./json_extract")
     echo '{
+    "aa": true,
     "a": "1",
     "b": [1, 2, 3],
+    "c": [ ],
+    "d": { },
     "5": {
         "a": 1
     }
-}' | awk "$code" -
+}' | json_main
 }
+
+# . json.sh; time cat test-data/b.json | json_main
+
+json_main(){
+    local code
+    code=$(cat "./json_walk" "./json_extract")
+    awk "$code" -
+}
+
+
