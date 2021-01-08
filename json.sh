fc86b8b786638fbc807fa53c5fb347a00ac83e2a
edwinjhlee <edwin.jh.lee@gmail.com>
2021-01-08 21:38:43 +0800

    parser ready

--- json.sh
+++ json.sh
@@ -2,8 +2,12 @@
 
 json_extract(){
     local code
-    code=$(cat "./json_walk")
+    code=$(cat "./json_walk" "./json_extract")
     echo '{
-        "a": "1"
-    }' | awk "$code" -
+    "a": "1",
+    "b": [1, 2, 3],
+    "5": {
+        "a": 1
+    }
+}' | awk "$code" -
 }
