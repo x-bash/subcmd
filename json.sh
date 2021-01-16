aa2b5dc11b3d304730096ee9d0952698af5ca748
edwinjhlee <edwin.jh.lee@gmail.com>
2021-01-16 17:04:27 +0800

    Every function prepared except pop

--- json.sh
+++ json.sh
@@ -97,6 +97,169 @@ json.awk.color1(){
     echo "${A[*]}"
 }
 
+# Using awk to parse ...
+# jo b                  == json.color b
+# jo b=[1,2,3]          == json.var b [1,2,3]
+# jo b.hi.c=5           == json.putkv
+# jo b.append(3)        == json.append b 3
+# jo b.shift()          == json.shift b
+# jo b.values()         == json.values b
+
+# JSON_AWK_PATH="$(xrc.which awk/json_walk)"
+JSON_AWK_PATH="./json_walk"
+json.var(){
+    local varname=${1:?Provide variable name} s
+    shift
+    s="$(awk -f "$JSON_AWK_PATH" <<<"$@")"
+    eval "$varname=\"\$s\""
+}
+
+json.color(){
+    local varname=${1:?Provide variable name} s
+    awk -v format=1 -v color=1 -f "$JSON_AWK_PATH" <<<"${!varname}"
+}
+
+json.append(){
+    local keypath=${1:?Provide variable name} opv1 opv2="${2:?Provide value}" varname s
+
+    varname=${keypath%%.*}
+    opv1=".${keypath#*.}"
+    [ "$varname" == "$keypath" ] && opv1="."
+    shift 2
+    
+    if [ "$varname" ]; then
+        s="$(awk -v op=append -v opv1="$opv1" -v opv2="$opv2" -f "$JSON_AWK_PATH" <<<"${!varname}")"
+    else
+        s="$(awk -v op=append -v opv1="$opv1" -v opv2="$opv2" -f "$JSON_AWK_PATH")"
+    fi
+    eval "$varname=\"\$s\""
+}
+
+json.prepend(){
+    local keypath=${1:?Provide variable name} opv1 opv2="${2:?Provide value}" varname s
+
+    varname=${keypath%%.*}
+    opv1=".${keypath#*.}"
+    [ "$varname" == "$keypath" ] && opv1="."
+    shift 2
+    
+    if [ "$varname" ]; then
+        s="$(awk -v op=prepend -v opv1="$opv1" -v opv2="$opv2" -f "$JSON_AWK_PATH" <<<"${!varname}")"
+    else
+        s="$(awk -v op=prepend -v opv1="$opv1" -v opv2="$opv2" -f "$JSON_AWK_PATH")"
+    fi
+    eval "$varname=\"\$s\""
+}
+
+: <<"DOCTEST"
+> b={a:1}
+> json.putkv b.b 2
+> echo "$b"
+DOCTEST
+json.putkv(){
+    local keypath=${1:?Provide variable name} opv1 opv2="${2:?Provide value}" varname s
+
+    varname=${keypath%%.*}
+    opv1=".${keypath#*.}"
+    [ "$varname" == "$keypath" ] && opv1="."
+    shift 2
+    
+    if [ "$varname" ]; then
+        s="$(awk -v dbg=1 -v op=putkv -v opv1="$opv1" -v opv2="$opv2" -f "$JSON_AWK_PATH" <<<"${!varname}")"
+    else
+        s="$(awk -v op=putkv -v opv1="$opv1" -v opv2="$opv2" -f "$JSON_AWK_PATH")"
+    fi
+    eval "$varname=\"\$s\""
+}
+
+: <<"DOCTEST"
+> b=[1,2,3]
+> json.values b
+1
+2
+3
+DOCTEST
+json.values(){
+    local keypath=${1:?Provide variable name} opv1 varname s
+
+    varname=${keypath%%.*}
+    opv1=".${keypath#*.}.*"
+    [ "$varname" == "$keypath" ] && opv1=".*"
+    shift 2
+    
+    if [ "$varname" ]; then
+        awk -v op=extract -v opv1="$opv1" -f "$JSON_AWK_PATH" <<<"${!varname}"
+    else
+        awk -v op=extract -v opv1="$opv1" -f "$JSON_AWK_PATH"
+    fi
+}
+
+: <<"DOCTEST"
+> b=[1,2,3]
+> json.del b.[0]
+> echo $b
+[2,3]
+DOCTEST
+json.del(){
+    local keypath=${1:?Provide variable name} opv1 varname s
+
+    varname=${keypath%%.*}
+    opv1=".${keypath#*.}"
+    [ "$varname" == "$keypath" ] && opv1="."
+    shift 2
+    
+    if [ "$varname" ]; then
+        s="$(awk -v op=del -v opv1="$opv1" -f "$JSON_AWK_PATH" <<<"${!varname}")"
+    else
+        s="$(awk -v op=del -v opv1="$opv1" -f "$JSON_AWK_PATH")"
+    fi
+    eval "$varname=\"\$s\""
+}
+
+: <<"DOCTEST"
+> b=[1,2,3]
+> json.shift b
+> echo $b
+[2,3]
+DOCTEST
+json.shift(){
+    local keypath=${1:?Provide variable name} opv1 varname s
+
+    varname=${keypath%%.*}
+    opv1=".${keypath#*.}"
+    [ "$varname" == "$keypath" ] && opv1="."
+    shift 2
+    
+    if [ "$varname" ]; then
+        s="$(awk -v dbg=1 -v op=shift -v opv1="$opv1" -f "$JSON_AWK_PATH" <<<"${!varname}")"
+    else
+        s="$(awk -v op=shift -v opv1="$opv1" -f "$JSON_AWK_PATH")"
+    fi
+    eval "$varname=\"\$s\""
+}
+
+: <<"DOCTEST"
+> b=[1,2,3]
+> json.pop b
+> echo $b
+[2,3]
+DOCTEST
+json.pop(){
+    local keypath=${1:?Provide variable name} opv1 varname s
+
+    varname=${keypath%%.*}
+    opv1=".${keypath#*.}"
+    [ "$varname" == "$keypath" ] && opv1="."
+    shift 2
+    
+    if [ "$varname" ]; then
+        s="$(awk -v op=pop -v opv1="$opv1" -f "$JSON_AWK_PATH" <<<"${!varname}")"
+    else
+        s="$(awk -v op=pop -v opv1="$opv1" -f "$JSON_AWK_PATH")"
+    fi
+    eval "$varname=\"\$s\""
+}
+
 json.array(){
     local s=
     while [ $# -gt 0 ]; do
@@ -122,6 +285,10 @@ json.escape(){
     a="$(echo "${a//$'\b'/\\b}")"
     a="$(echo "${a//\"/\\\"}")"
     echo "\"$a\""
+}
+
+json.unescape(){
+    eval echo "$@"
 }
 
 # Reference: https://github.com/jpmens/jo/blob/master/jo.md
