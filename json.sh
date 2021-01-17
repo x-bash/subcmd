d2eb9845af3b80b721009ace5e514cf76cfe7bda
edwinjhlee <edwin.jh.lee@gmail.com>
2021-01-17 15:39:00 +0800

    bugfix

--- json.sh
+++ json.sh
@@ -1,102 +1,5 @@
 #! /usr/bin/env bash
 
-# . json.sh; json_extract
-
-# jq . <<< '{
-#     "aa": true,
-#     "a": "1\"\\",
-#     "c": -1.3,
-#     "b": [1, 2, 3],
-#     "c": [ { "member": 1} ],
-#     "d": { },
-#     "5": {
-#         "a": 1
-#     }
-# }'
-
-json_extract(){
-    json_main <<< '{
-    a: fff, 
-    "aa": true,
-    "aa1": false,
-    "a": "1\"\\",   // hi
-    "c": -1.3,      /* hello */
-    # hello
-    "b": [1, 2, 3],
-    "c": [ { "member": 1} ],
-    "d": { },
-    "5": {
-        "a": 1
-    }
-}'
-}
-
-# . json.sh; time json_main  <test-data/b.json >/dev/null
-# . json.sh; time cat test-data/b.json | json_main
-
-json_main(){
-    awk "$(cat "./json_walk")" -
-}
-
-
-json_acc(){
-    # awk "$(cat "./json_acc")" -
-    awk -f ./json_acc -
-}
-
-json_acc0(){
-    json_acc <<< '{
-    "aa": true,
-    "aa1": false,
-    "a": "1\"\\",
-    "c": -1.3,
-    "b": [1, 2, 3],
-    "c": [ { "member": 1} ],
-    "d": { },
-    "5": {
-        "a": 1
-    }
-}'
-}
-
-
-# Scheme 1:
-# out_color_key = "\033[0;35m"
-# out_color_string = "\033[0;34m"
-# out_color_number = "\033[0;32m"
-# out_color_null = "\033[0;33m"   # "\033[0;31m"
-# out_color_true = "\033[7;32m"
-# out_color_false = "\033[7;31m"
-
-# Scheme 2:
-# out_color_key = "\033[1;34m"
-# out_color_string = "\033[0;33m"
-# out_color_number = "\033[1;35m"
-# out_color_null = "\033[0;31m"
-# out_color_true = "\033[7;32m"
-# out_color_false = "\033[7;31m"
-
-# Scheme 3:
-# out_color_key = "\033[1;33m"
-# out_color_string = "\033[0;34m"
-# out_color_number = "\033[0;35m"
-# out_color_null = "\033[0;31m"
-# out_color_true = "\033[7;32m"
-# out_color_false = "\033[7;31m"
-
-json.awk.color1(){
-    local IFS=$' '
-    A=(
-    -v out_color_key="\033[0;35m"
-    -v out_color_string="\033[0;34m"
-    -v out_color_number="\033[0;32m"
-    -v out_color_null="\033[0;33m"
-    -v out_color_true="\033[7;32m"
-    -v out_color_false="\033[7;31m"
-    )
-    echo "${A[*]}"
-}
-
 # Using awk to parse ...
 # jo b                  == json.color b
 # jo b=[1,2,3]          == json.var b [1,2,3]
@@ -155,7 +58,7 @@ json.prepend(){
 
 : <<"DOCTEST"
 > b={a:1}
-> json.putkv b.b 2
+> json.put b.b 2
 > echo "$b"
 DOCTEST
 json.put(){
@@ -167,7 +70,7 @@ json.put(){
     shift 2
     
     if [ "$varname" ]; then
-        s="$(awk -v dbg=1 -v op=put -v opv1="$opv1" -v opv2="$opv2" -f "$JSON_AWK_PATH" <<<"${!varname}")"
+        s="$(awk -v op=put -v opv1="$opv1" -v opv2="$opv2" -f "$JSON_AWK_PATH" <<<"${!varname}")"
     else
         s="$(awk -v op=put -v opv1="$opv1" -v opv2="$opv2" -f "$JSON_AWK_PATH")"
     fi
@@ -185,14 +88,29 @@ json.values(){
     local keypath=${1:?Provide variable name} opv1 varname s
 
     varname=${keypath%%.*}
-    opv1=".${keypath#*.}.*"
-    [ "$varname" == "$keypath" ] && opv1=".*"
+    opv1=".${keypath#*.}"
+    [ "$varname" == "$keypath" ] && opv1=""
     shift 2
     
     if [ "$varname" ]; then
+        awk -v op=values -v opv1="$opv1" -f "$JSON_AWK_PATH" <<<"${!varname}"
+    else
+        awk -v op=values -v opv1="$opv1" -f "$JSON_AWK_PATH"
+    fi
+}
+
+json.query(){
+    local keypath=${1:?Provide variable name} opv1 varname s
+
+    varname=${keypath%%.*}
+    opv1=".${keypath#*.}"
+    [ "$varname" == "$keypath" ] && opv1="."
+    shift 2
+
+    if [ "$varname" ]; then
         awk -v op=extract -v opv1="$opv1" -f "$JSON_AWK_PATH" <<<"${!varname}"
     else
-        awk -v op=extract -v opv1="$opv1" -f "$JSON_AWK_PATH"
+        awk -v op=extract -v opv1="$opv1" -f "$JSON_AWK_PATH" -
     fi
 }
 
@@ -233,7 +151,7 @@ json.shift(){
     shift 2
     
     if [ "$varname" ]; then
-        s="$(awk -v dbg=1 -v op=shift -v opv1="$opv1" -f "$JSON_AWK_PATH" <<<"${!varname}")"
+        s="$(awk -v op=shift -v opv1="$opv1" -f "$JSON_AWK_PATH" <<<"${!varname}")"
     else
         s="$(awk -v op=shift -v opv1="$opv1" -f "$JSON_AWK_PATH")"
     fi
@@ -349,4 +267,43 @@ json.dict(){
     done
     printf "\n}"
 }
+
+
+# Scheme 1:
+# out_color_key = "\033[0;35m"
+# out_color_string = "\033[0;34m"
+# out_color_number = "\033[0;32m"
+# out_color_null = "\033[0;33m"   # "\033[0;31m"
+# out_color_true = "\033[7;32m"
+# out_color_false = "\033[7;31m"
+
+# Scheme 2:
+# out_color_key = "\033[1;34m"
+# out_color_string = "\033[0;33m"
+# out_color_number = "\033[1;35m"
+# out_color_null = "\033[0;31m"
+# out_color_true = "\033[7;32m"
+# out_color_false = "\033[7;31m"
+
+# Scheme 3:
+# out_color_key = "\033[1;33m"
+# out_color_string = "\033[0;34m"
+# out_color_number = "\033[0;35m"
+# out_color_null = "\033[0;31m"
+# out_color_true = "\033[7;32m"
+# out_color_false = "\033[7;31m"
+
+json.awk.color1(){
+    local IFS=$' '
+    A=(
+    -v out_color_key="\033[0;35m"
+    -v out_color_string="\033[0;34m"
+    -v out_color_number="\033[0;32m"
+    -v out_color_null="\033[0;33m"
+    -v out_color_true="\033[7;32m"
+    -v out_color_false="\033[7;31m"
+    )
+    echo "${A[*]}"
+}
+
 
