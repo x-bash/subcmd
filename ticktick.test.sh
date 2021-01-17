a417087667247bedfc922aab76f7db419396eaa8
edwinjhlee <edwin.jh.lee@gmail.com>
2021-01-17 16:38:34 +0800

    Add ticktick test

--- ticktick.test.sh
+++ ticktick.test.sh
@@ -10,24 +10,26 @@ owner='{
 developer="{
     Owner: $owner,
     Contributors: [
-        Li Junhao,
-        Li Tinghui,
-        Tang Zhiwen
+        Tang Zhiwen,
+        Li Tinghui
     ]
 }"
 
 function printContributors(){
-    # local IFS=$'\n'
-    for employee in "$(json.values developer.Contributors )"; do
-        echo "$employee"
+    echo "------------------"
+    echo "Contributors:"
+    local IFS=$'\n'
+    for employee in $(json.values developer.Contributors ); do
+        echo "--- $employee"
     done
+    echo "------------------"
 }
 
 echo Base assignments
 
 json.put developer.Candidates '[ "YLJ", "Wang Li", "ZLX" ]'
 
-echo "$(json.values developer.Candidates)"
+json.values developer.Candidates
 
 newContributor="Zhang Chi"
 echo "Pushed a new , $newContributor onto the array"
@@ -37,7 +39,12 @@ json.color developer
 printContributors
 
 person0=$(json.query developer.Contributors.[0])
-echo "$person0"
+echo -e "First Contributor:\t $person0"
+
+json.color developer
 
 json.shift developer.Contributors
+printContributors
+
+json.pop developer.Contributors
 printContributors
